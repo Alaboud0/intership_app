0d454eaac79b50c3fc42669c4ccfa4aae0f8c253
Alaboud0 <101182840+Alaboud0@users.noreply.github.com>
2024-03-20 10:06:50 +0300

    app_intern

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
+
+module.exports={
+  env:{MONGO_URI:"mongodb+srv://okba:okbaqwe123@cluster0.tn9n6.mongodb.net/okba?retryWrites=true&w=majority"}
+}
